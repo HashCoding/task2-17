b9ab114134f8696ed9e569233eea3e9a5006e106
rccoder <rccoder.net@gmail.com>
2016-03-25 22:56:50 +0800

    day and month show

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 (function() {
     
+    var _ = {
+        addHandle: function (element, type, handler) {
+            if(element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else if(element.attachEvent) {
+                element.attachEvent('on'+type, handler);
+            } else {
+                element['on'+type] = handler;
+            }
+        }
+    }    
+
     /* 数据格式演示
     var aqiSourceData = {
     "北京": {
@@ -44,7 +56,22 @@
     "沈阳": randomBuildData(500)
     };
 
+    /**
+     *  获取当前时间粒度
+     */
+    var getGraTime = function () {
+        var graTime = document.getElementsByName('gra-time');
+        
+        for(var i = 0, l = graTime.length; i < l; i++) {
+            if(graTime[i].checked) {
+                return graTime[i].value;
+            }
+        }
+    }
+    
     // 用于渲染图表的数据
+    // {2016-01-01: 1, 2016-01-02: 2....}
+    // {1月: 231, 2月: 303, 3月: 200...}
     var chartData = {};
 
     // 记录当前页面的表单选项
@@ -57,18 +84,36 @@
      * 渲染图表
      */
     function renderChart() {
-
+        console.log('renderChart is ok');
+        
+        var str = '';
+        for(var i in chartData) {
+            str += '<div class="box ' + pageState.nowGraTime + '">';
+            str += '<div class="data" style="height: ' + chartData[i] + 'px" title="' + i + ',' + chartData[i] + '"></div>';
+            str += '</div></div>';
+        }
+        
+        var aqiChartWrap = document.getElementsByClassName('aqi-chart-wrap')[0];
+        aqiChartWrap.innerHTML = str;
     }
 
     /**
      * 日、周、月的radio事件点击时的处理函数
      */
-    function graTimeChange() {
-    // 确定是否选项发生了变化 
-
-    // 设置对应数据
-
-    // 调用图表渲染函数
+    var graTimeChange = function() {
+        console.log('gra time change is ok')
+        // 确定是否选项发生了变化
+        
+        var nowGraTime = getGraTime();
+        console.log('now ' + nowGraTime + ' is checked')
+        if(nowGraTime === pageState.nowGraTime) {
+            return;
+        } else {
+            // 设置对应数据
+            initAqiChartData();
+            // 调用图表渲染函数
+            renderChart();
+        }
     }
 
     /**
@@ -85,9 +130,15 @@
     /**
      * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
      */
-    function initGraTimeForm() {
-        var graTime = document.getElementById('gra-time');
+    var initGraTimeForm = function() {
+        console.log('initGraTimeForm is ok');
         
+        var graTime = document.getElementsByName('gra-time');
+        
+        for(var i = 0, l = graTime.length; i < l; i++) {
+            _.addHandle(graTime[i], 'click', graTimeChange);
+            console.log('radio click event is ok');
+        }
     }
 
     /**
@@ -100,21 +151,75 @@
 
     }
 
+
     /**
      * 初始化图表需要的数据格式
      */
-    function initAqiChartData() {
-    // 将原始的源数据处理成图表需要的数据格式
-    // 处理好的数据存到 chartData 中
+    var initAqiChartData = function() {
+        console.log('init aqichartdata is ok')
+       
+        var nowGraTime = getGraTime();
+        var nowSelectCity = document.getElementById('city-select').value;
+        // 保存状态
+        pageState.nowGraTime = nowGraTime;
+        pageState.nowSelectCity = nowSelectCity;
+        
+        console.log(pageState)
+        
+        // 将原始的源数据处理成图表需要的数据格式
+        // 处理好的数据存到 chartData 中
+        if(nowGraTime === 'day') {
+            chartData = aqiSourceData[nowSelectCity];
+             // {2016-01-01: 1, 2016-01-02: 2....}
+            // console.log(chartData);
+        } else if(nowGraTime === 'week') {
+            // 好难，先等等
+            var tepData = aqiSourceData[nowSelectCity];
+            for (var i in tepData) {
+                
+            }
+        } else if(nowGraTime === 'month') {
+            chartData = {};
+            var tepData = aqiSourceData[nowSelectCity];
+            var count = 0, total = 0, flag = -1;
+            // flag 代表现在的月份, 当新的日子不是之前的话要计算的数归0,并且存储上月的数据
+            for (var day in tepData) {
+                if(flag === -1) {
+                    flag = parseInt(day.split('-')[1]);
+                } else if(parseInt(day.split('-')[1]) != flag) {
+                    
+                    // 存储上月数据, 四舍五入
+                    chartData[flag + '月'] = Math.round(total/count);
+                    
+                    flag = parseInt(day.split('-')[1]);
+                    count = 0;
+                    total = 0;
+                }
+                
+                // 新的一天
+                count ++;
+                total += tepData[day];
+            }
+            
+            // 最后一月的数据
+            chartData[flag + '月'] = Math.round(total/count);
+            
+            // console.log(chartData);
+            // {1月: 231, 2月: 303, 3月: 200...}
+        }
     }
+
 
     /**
      * 初始化函数
      */
     var init = function() {
-        initGraTimeForm()
+        console.log('init is ok')
+        
+        initGraTimeForm();
         initCitySelector();
         initAqiChartData();
+        renderChart();
     }
 
     init();
